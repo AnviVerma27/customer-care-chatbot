538ebad6a91e791fb49054bd068a328fc286f19a
Anvi Verma <114463178+AnviVerma27@users.noreply.github.com>
2023-11-09 14:09:42 +0530

    changes

--- app.py
+++ app.py
@@ -9,65 +9,57 @@ search_term = st.text_input("How may I help you ?", value="",placeholder='Ask yo
 chat_history = st.session_state.get("chat_history", [])
 
 if st.button("Search"):
-    try:
-        if search_term:
-            url = "https://api.vectara.io:443/v1/query"
-            headers = {
-                "x-api-key": st.secrets.api_key,
-                "customer-id": st.secrets.customer_id}
+    if search_term:
+        url = "https://api.vectara.io:443/v1/query"
+        headers = {
+            "x-api-key": st.secrets.api_key,
+            "customer-id": st.secrets.customer_id}
     
-            payload = {
-                "query": [
+        payload = {
+            "query": [
+                {
+                "query": search_term,
+                "queryContext": "",
+                "start": 0,
+                "numResults": 10,
+                "contextConfig": { 
+                    "charsBefore": 0,
+                    "charsAfter": 0,
+                    "sentencesBefore": 2,
+                    "sentencesAfter": 2,
+                    "startTag": "%START_SNIPPET%",
+                    "endTag": "%END_SNIPPET%"
+                },
+                "corpusKey": [
                     {
-                    "query": search_term,
-                    "queryContext": "",
-                    "start": 0,
-                    "numResults": 10,
-                    "contextConfig": {
-                        "charsBefore": 0,
-                        "charsAfter": 0,
-                        "sentencesBefore": 2,
-                        "sentencesAfter": 2,
-                        "startTag": "%START_SNIPPET%",
-                        "endTag": "%END_SNIPPET%"
-                    },
-                    "corpusKey": [
-                        {
-                            "customerId": st.secrets.customer_id,
-                            "corpusId": st.secrets.corpus_id,
-                            "semantics": 0,
-                            "metadataFilter": "",
-                                "lexicalInterpolationConfig": {
-                            "lambda": 0.025
-                            },
-                            "dim": []
-                        }
-                    ],
-                    "summary": [
-                        {
-                            "maxSummarizedResults": 2,
-                            "responseLang": "eng",
-                            "summarizerPromptName": "vectara-summary-ext-v1.2.0"
-                        }
-                    ]
-                }
-            ]
-        }
-    
-        response = requests.post(url, headers=headers, json=payload)
-        search_results = json.loads(response.text)
-        summary = search_results["responseSet"][0]["summary"][0]["text"]
-        st.write(summary)
-        chat_history.append(f"**You:** {search_term}")
-        chat_history.append(f"**Bot:** {summary}")
-        chat_history.append("---------------------------------------------")
+                        "customerId": st.secrets.customer_id,
+                        "corpusId": st.secrets.corpus_id,
+                        "semantics": 0,
+                        "metadataFilter": "",
+                            "lexicalInterpolationConfig": {
+                                "lambda": 0.025
+                        },
+                        "dim": []
+                    }
+                ],
+                "summary": [
+                    {
+                        "maxSummarizedResults": 2,
+                        "responseLang": "eng",
+                        "summarizerPromptName": "vectara-summary-ext-v1.2.0"
+                    }
+                ]
+            }
+        ]
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    search_results = json.loads(response.text)
+    summary = search_results["responseSet"][0]["summary"][0]["text"]
+    st.write(summary)
+    chat_history.append(f"**You:** {search_term}")
+    chat_history.append(f"**Bot:** {summary}")
+    chat_history.append("---------------------------------------------")
                 
-    except:
-        error_message = "enter the query first"
-        st.text(error_message)
-        chat_history.append(f"**You:** {search_term}")
-        chat_history.append(f"**Bot:** {error_message}")
-        chat_history.append("---------------------------------------------")
         
 st.sidebar.title("Chat History")
 chat_history_markdown = "\n\n".join(chat_history)
